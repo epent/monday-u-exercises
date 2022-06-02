42f9021b4dbf5f982f69ad9874d9bc5ce31ad979
epent <helene.pentina@gmail.com>
2022-06-02 14:01:38 +0300

    add 'delete'

--- src/ex3/node-cli.js
+++ src/ex3/node-cli.js
@@ -2,9 +2,10 @@ import fs from "fs/promises";
 import { Command } from "commander";
 const program = new Command();
 
-async function writeToFile(item) {
+async function writeToFile(item, addNewLine, flag) {
   try {
-    await fs.writeFile("src/ex3/test.txt", `\n${item}`, { flag: "a+" });
+    const newLine = addNewLine ? "\n" : "";
+    await fs.writeFile("src/ex3/test.txt", `${item}${newLine}`, { flag: flag });
   } catch (err) {
     console.log(err);
   }
@@ -29,8 +30,8 @@ program
   .description("Add item to the list")
   .argument("<string>", "item")
   .action((item) => {
-    console.log(`Result: ${item}`);
-    writeToFile(item);
+    console.log(`Successfully added: ${item}`);
+    writeToFile(item, true, "a+");
   });
 
 program
@@ -38,7 +39,21 @@ program
   .description("Get all items on the list")
   .action(async () => {
     const data = await readFromFile();
-    console.log(`ToDo list: ${data}`);
+    console.log(`ToDo list: \n${data}`);
+  });
+
+program
+  .command("delete")
+  .description("Delete item from the list")
+  .argument("<number>", "item index")
+  .action(async (index) => {
+    const data = await readFromFile();
+    const array = data.toString().split("\n");
+
+    array.splice(index, 1);
+    array.pop();
+
+    array.length > 0 ? writeToFile(array.join("\n")) : writeToFile("");
   });
 
 program.parse();
