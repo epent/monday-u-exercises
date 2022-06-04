fb2fc9b7850d72945ad1e2eb9f5fc02502589e51
epent <helene.pentina@gmail.com>
2022-06-04 16:22:29 +0300

    fix inquirer

--- src/ex3/inquirer.js
+++ src/ex3/inquirer.js
@@ -8,60 +8,66 @@ import {
   sortItems,
 } from "./commands.js";
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "command",
-      message: "Which command would you like to run?",
-      choices: [
-        "add new item",
-        "get all items",
-        "delete item by index",
-        "delete item by name",
-        "delete all items",
-        "sort items by name",
-        "exit",
-      ],
-    },
-    {
-      type: "input",
-      name: "item",
-      message: "Type your new todo or pokemon id",
-      when: (answer) => answer.command === "add new item",
-    },
-    {
-      type: "input",
-      name: "delete_index",
-      message: "Type index of item you want to delete",
-      when: (answer) => answer.command === "delete item by index",
-    },
-    {
-      type: "input",
-      name: "delete_name",
-      message: "Type name of item you want to delete",
-      when: (answer) => answer.command === "delete item by name",
-    },
-  ])
-  .then(async (answer) => {
-    switch (answer.command) {
-      case "add new item":
-        addItem(answer.item);
-        break;
-      case "get all items":
-        getItem();
-        break;
-      case "delete item by index":
-        deleteItem(answer.delete_index);
-        break;
-      case "delete item by name":
-        deleteItem(answer.delete_name);
-        break;
-      case "delete all items":
-        deleteAllItems();
-        break;
-      case "sort items by name":
-        sortItems();
-        break;
-    }
-  });
+const questions = [
+  {
+    type: "list",
+    name: "command",
+    message: "Which command would you like to run?",
+    choices: [
+      "add new item",
+      "get all items",
+      "delete item by index",
+      "delete item by name",
+      "delete all items",
+      "sort items by name",
+      "exit",
+    ],
+  },
+  {
+    type: "input",
+    name: "item",
+    message: "Type your new todo or pokemon id",
+    when: (answer) => answer.command === "add new item",
+  },
+  {
+    type: "input",
+    name: "delete_index",
+    message: "Type index of item you want to delete",
+    when: (answer) => answer.command === "delete item by index",
+  },
+  {
+    type: "input",
+    name: "delete_name",
+    message: "Type name of item you want to delete",
+    when: (answer) => answer.command === "delete item by name",
+  },
+];
+
+async function callInquirer() {
+  const answer = await inquirer.prompt(questions);
+  switch (answer.command) {
+    case "add new item":
+      addItem(answer.item);
+      break;
+    case "get all items":
+      getItem();
+      break;
+    case "delete item by index":
+      deleteItem(answer.delete_index);
+      break;
+    case "delete item by name":
+      deleteItem(answer.delete_name);
+      break;
+    case "delete all items":
+      deleteAllItems();
+      break;
+    case "sort items by name":
+      sortItems();
+      break;
+    case "exit":
+      return;
+  }
+  return callInquirer();
+}
+
+callInquirer();
