d38932e40973c1602965c438a2c8735095003e26
epent <helene.pentina@gmail.com>
2022-06-11 11:43:58 +0300

    install express,body-parser, create server

--- src/ex4/server.js
+++ src/ex4/server.js
@@ -1,1 +1,12 @@
 // Express boilerplate, hosting the `dist` file, connecting to the routes
+
+import { app } from "./app";
+import { http } from "htpp";
+
+const port = 8080;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log("Server started on port", port);
+});
