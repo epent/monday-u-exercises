b9564e3d5bae6881cd1a39d2e345438527e17fd4
epent <helene.pentina@gmail.com>
2022-06-11 18:36:02 +0300

    refactor utils

--- src/ex4/utils/utils.js
+++ src/ex4/utils/utils.js
@@ -1,21 +1,23 @@
 import fs from "fs/promises";
 
-export async function writeToFile(item, addNewLine, flag) {
+async function writeToFile(item, addNewLine, flag) {
   try {
     const newLine = addNewLine ? "\n" : "";
     await fs.writeFile("src/ex4/ItemList.txt", `${item}${newLine}`, {
       flag: flag,
     });
   } catch (err) {
-    console.log(err);
+    console.error(`Failed to write to file ${error.message}`);
   }
 }
 
-export async function readFromFile() {
+async function readFromFile() {
   try {
     const data = await fs.readFile("src/ex4/ItemList.txt");
-    return data;
+    return data.toString();
   } catch (err) {
-    console.log(err);
+    console.error(`Got an error trying to read the file: ${err.message}`);
   }
 }
+
+export { writeToFile, readFromFile };
